cf733847dc669832882cbd2d3b82547c2131ada6
Cui Bingbing <752856108@qq.com>
2023-12-21 14:50:34 +0800

    feat:发布tui组件

--- build-ui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm config set registry http://10.11.6.81:8989/nexus/content/groups/npm-all/
-
-npm i
-
-npm run build
-
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  endpoint: {
-    IndustryScreen: '/tui-element/',
-  },
-};
